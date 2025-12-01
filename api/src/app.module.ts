a6b201d9b80e6f4c5a198daf7d49da3ccf0189ae
atkinsonholly <atkinsonholly@gmail.com>
2025-12-01 14:10:54 +0000

    add: health controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DecodeController } from './decode/decode.controller';
 import { RiscVDecoderService } from './decode/risc-v-decoder.service';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [],
-  controllers: [DecodeController],
+  controllers: [DecodeController, HealthController],
   providers: [RiscVDecoderService],
 })
 export class AppModule {}
