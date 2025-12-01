0ba233c8446a716c05eecf0cf2ece6945431f7ea
atkinsonholly <atkinsonholly@gmail.com>
2025-12-01 14:15:56 +0000

    add: cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const allowedOrigin = process.env.FRONTEND_ORIGIN || 'http://localhost:5173';
+  app.enableCors({
+    origin: allowedOrigin,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
